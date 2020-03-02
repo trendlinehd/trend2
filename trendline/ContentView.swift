c63a2d02fddaa0d0b64fd432ffae7c13069db4fa
jithin eringat <e.jithin5@gmail.com>
2020-03-01 19:11:27 -0800

    Initial Commit

---
+++ trendline/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  trendline
+//
+//  Created by Jithin Eringat on 1/3/20.
+//  Copyright © 2020 Jithin Eringat. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ trendlineTests/trendlineTests.swift
@@ -0,0 +1,34 @@
+//
+//  trendlineTests.swift
+//  trendlineTests
+//
+//  Created by Jithin Eringat on 1/3/20.
+//  Copyright © 2020 Jithin Eringat. All rights reserved.
+//
+
+import XCTest
+@testable import trendline
+
+class trendlineTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
